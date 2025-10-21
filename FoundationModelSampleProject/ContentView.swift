b7f4f122e62a55508d1024763fe87332f9671e0b
Ngordon68 <ngordon688@gmail.com>
2025-10-21 14:06:32 -0400

    Initial Commit

---
+++ FoundationModelSampleProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoundationModelSampleProject
+//
+//  Created by Nick Gordon on 10/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoundationModelSampleProject/FoundationModelSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelSampleProjectApp.swift
+//  FoundationModelSampleProject
+//
+//  Created by Nick Gordon on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
